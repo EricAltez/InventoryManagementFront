a89484c60bf29a68718b87c4fc0c27dc01b0e1cc
ericaltez <ericaltez1111@gmail.com>
2025-04-27 17:45:12 -0300

    add input fields reset on add to list

--- inventoryfront/src/components/sales.tsx
+++ inventoryfront/src/components/sales.tsx
@@ -12,6 +12,8 @@ export default function Sales() {
       ...prevList,
       { productId: parseInt(productId), quantity: parseInt(quantity) },
     ]);
+    setProductId("");
+    setQuantity("");
   };
 
   const makeSale = async (event: any) => {
@@ -20,9 +22,15 @@ export default function Sales() {
 
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>
-        <div>
-          <form onSubmit={addToList}>
+      <div className="grid grid-cols-3 items-start gap-16 p-4 border-solid border-2 border-black rounded-lg">
+        {/*this form is changin his parent size*/}
+        {/*add real object values*/}
+        {/*define the fields to be used*/}
+        <form
+          onSubmit={addToList}
+          className="items-center justify-items-center"
+        >
+          <div>
             <div>
               <Input
                 placeholder="product id"
@@ -39,27 +47,32 @@ export default function Sales() {
                 onChange={(value) => setQuantity(value)}
               />
             </div>
-            <div>
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                type="submit"
-                onClick={addToList}
-              >
-                Add to sale list
-              </button>
-            </div>
-          </form>
+          </div>
+          <div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+              onClick={addToList}
+            >
+              Add to sale list
+            </button>
+          </div>
+        </form>
 
-          <div className="mt-8 border-solid border-2 border-black p-4 rounded-lg">
-            <h2 className="text-lg text-black font-bold">Sale List</h2>
-            <div className="mt-4 text-black">
-              {saleList.map((item, index) => (
-                <div key={index} className="flex justify-between">
-                  <span>Product ID: {item.productId}</span>
-                  <span>Quantity: {item.quantity}</span>
-                </div>
-              ))}
+        {/*List*/}
+        <div className="border-solid border-2 border-black p-4 rounded-lg">
+          <h2 className="text-lg text-black font-bold p-2">Sale List</h2>
+          <div className=" text-black">
+            <div className="flex justify-between">
+              <span className="p-2">Product ID: </span>
+              <span className="p-2">Quantity: </span>
             </div>
+            {saleList.map((item, index) => (
+              <div key={index} className="flex justify-between">
+                <span className="p-2">{item.productId}</span>
+                <span className="p-2">{item.quantity}</span>
+              </div>
+            ))}
           </div>
         </div>
 
